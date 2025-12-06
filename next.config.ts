4882f5d56d61efe470e22870f001994ab1f2e0b9
Bandi Murali Chowdary <muralichowdary386@gmail.com>
2025-12-06 15:05:38 +0530

    Fix next.config.ts build error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-expect-error - standard next.js config for ignoring linting
   eslint: {
     ignoreDuringBuilds: true,
   },
